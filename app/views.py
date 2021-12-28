b9c3cb07a39e6309f81b9c32ad8a61b8ddad1da5
junhwan <ujunhwan@gmail.com>
2021-12-28 17:10:45 +0900

    [feat] hello view 생성

--- app/views.py
+++ app/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+
+class HelloView(APIView):
+    def get(self, request):
+        return Response("GET Hello", status=200)
+
+    def post(self, request):
+        return Response("POST Hello", status=200)
+
+    def patch(self, request):
+        return Response("PATCH Hello", status=200)
+
+    def delete(self, request):
+        return Response("DELETE Hello", status=200)
--- server/settings/base.py
+++ server/settings/base.py
@@ -13,6 +13,8 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 SECRET_KEY = env('DJANGO_SECRET_KEY')
 DEBUG = env('DEBUG')
 
+APPEND_SLASH = False
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
