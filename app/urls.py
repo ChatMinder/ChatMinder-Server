8ab4a12f260a4f7857cb58a51c3c353a0da6b891
chaeri93 <chaeri93@ewhain.net>
2021-12-31 10:58:23 +0900

    [chore] url 수정

--- app/urls.py
+++ app/urls.py
@@ -2,6 +2,7 @@ from django.urls import include, path
 from rest_framework import routers
 
 from app.views import *
+from app.views_memo import MemoViewSet
 
 router = routers.DefaultRouter()
 router.register(r'memos', MemoViewSet)
