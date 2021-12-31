06a075475a13a6e5b655fd51383d7f325c4bdc2c
chaeri93 <chaeri93@ewhain.net>
2021-12-31 11:48:59 +0900

    [feat] 모아보기 filter 추가

--- app/views_memo.py
+++ app/views_memo.py
@@ -8,9 +8,11 @@ from app.serializers_memo import MemoSerializer
 
 class MemoFilter(FilterSet):
     memo_text = filters.CharFilter(field_name='memo_text', lookup_expr="icontains")
-    is_marked = filters.BooleanFilter(field_name='is_marked')
-    image_null = filters.BooleanFilter(field_name='image', method='is_image_null')
-    link_null = filters.BooleanFilter(field_name='link', method='is_link_null')
+    link = filters.CharFilter(field_name='link', lookup_expr="icontains")
+    is_marked = filters.BooleanFilter(field_name='is_marked') #뷱마크 모아보기
+    image_null = filters.BooleanFilter(field_name='image', method='is_image_null') #이미지 모아보기
+    link_null = filters.BooleanFilter(field_name='link', method='is_link_null') #링크 모아보기
+    is_text_null = filters.BooleanFilter(field_name='memo_text', method='is_text_null') #텍스트 모아보기
 
     class Meta:
         model = Memo
@@ -27,6 +29,12 @@ class MemoFilter(FilterSet):
             return queryset.filter(link__isnull=True)
         else:
             return queryset.filter(link__isnull=False)
+
+    def is_text_null(self, queryset, text, value):
+        if value:
+            return queryset.filter(text__isnull=True)
+        else:
+            return queryset.filter(text__isnull=False)
 
 
 class MemoViewSet(ModelViewSet):
