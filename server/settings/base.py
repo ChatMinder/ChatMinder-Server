565e7912c458cf4f1852d53682f3fc338501d2c1
junhwan <ujunhwan@gmail.com>
2022-01-04 16:24:52 +0900

    [feat] 토큰 유지 시간 변경 (2days)

--- server/settings/base.py
+++ server/settings/base.py
@@ -94,8 +94,8 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': datetime.timedelta(minutes=30),
-    'REFRESH_TOKEN_LIFETIME': datetime.timedelta(days=1),
+    'ACCESS_TOKEN_LIFETIME': datetime.timedelta(days=2),
+    'REFRESH_TOKEN_LIFETIME': datetime.timedelta(days=30),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': False,
     'UPDATE_LAST_LOGIN': True,
