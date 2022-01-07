c0339da2097d496e019424e996617c0013088209
Junhwan Yu <ujunhwan@gmail.com>
2022-01-07 13:40:46 +0900

    [feat] 이미지 부분 기능 추가 및 리팩토링 (#29)
    * [feat] image field에 User fk 추가
    
    * [feat] custom exception 추가
    
    * [feat] s3 관련 메서드 추가
    
    * [feat] storage, exception import 추가
    
    * [feat] 유저 인증 메서드 분리
    
    * [refactor] try-catch 구조로 리팩토링

--- app/storages.py
+++ app/storages.py
@@ -10,3 +10,19 @@ def get_s3_connection():
         aws_secret_access_key=env('AWS_SECRET_ACCESS_KEY'),
         region_name=env('REGION_NAME'),
     )
+
+
+def s3_upload_image(image_file, resource_url):
+    s3 = get_s3_connection()
+    s3.upload_fileobj(
+        image_file,
+        env('S3_BUCKET_NAME'),
+        resource_url,
+        ExtraArgs={
+            "ContentType": image_file.content_type,
+        }
+    )
+
+def s3_delete_image(image):
+    s3 = get_s3_connection()
+    s3.Object(env('S3_BUCKET_NAME'), image.url).delete()
