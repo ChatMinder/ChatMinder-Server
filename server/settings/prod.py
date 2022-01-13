fe2130012cb3ce50deeace2e8b58b972b4c61e1a
junhwan <ujunhwan@gmail.com>
2022-01-13 14:46:22 +0900

    [fix] logger 배포환경에서만 실행하게 설정 변경

--- server/settings/prod.py
+++ server/settings/prod.py
@@ -13,3 +13,53 @@ DATABASES = {
         'PORT': env('DATABASE_PORT'),
     }
 }
+
+
+# Log config
+
+LOG_DIR = '/var/log/django'
+if not os.path.exists(LOG_DIR):
+    LOG_DIR = os.path.join(BASE_DIR, '.log')
+    os.makedirs(LOG_DIR, exist_ok=True)
+
+subprocess.call(['chmod', '755', LOG_DIR])
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse',
+        },
+        'require_debug_true': {
+            '()': 'django.utils.log.RequireDebugTrue',
+        },
+    },
+    'formatters': {
+        'django.server': {
+            'format': '[%(asctime)s] %(message)s',
+        }
+    },
+    'handlers': {
+        'console': {
+            'level': 'INFO',
+            'filters': ['require_debug_true'],
+            'class': 'logging.StreamHandler',
+        },
+        'file_error': {
+            'class': 'logging.handlers.RotatingFileHandler',
+            'level': 'ERROR',
+            'formatter': 'django.server',
+            'backupCount': 10,
+            'filename': os.path.join(LOG_DIR, 'error.log'),
+            'maxBytes': 10485760,
+        }
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console', 'file_error'],
+            'level': 'INFO',
+            'propagate': True,
+        },
+    }
+}
