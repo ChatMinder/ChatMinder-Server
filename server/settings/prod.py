1ef2334f800357aa303fecc01dcb6d01a52092d7
junhwan <ujunhwan@gmail.com>
2022-01-26 14:25:41 +0900

    [feat] emoji 적용을 위한 django ORM 설정 변경

--- server/settings/prod.py
+++ server/settings/prod.py
@@ -12,6 +12,11 @@ DATABASES = {
         'PASSWORD': env('DATABASE_PASSWORD'),
         'HOST': env('DATABASE_HOST'),
         'PORT': env('DATABASE_PORT'),
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+            'charset': 'utf8mb4',
+            'use_unicode': True,
+        },
     }
 }
 
